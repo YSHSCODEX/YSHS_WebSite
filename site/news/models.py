d2d3fe71b7e97180519615ab4a8d56cdc8390c17
gitsokyumin <kyumin3089@gmail.com>
2024-11-11 20:14:41 +0900

    2024.11.11

--- site/news/models.py
+++ site/news/models.py
@@ -4,4 +4,4 @@ class contents(models.Model):
     subject = models.CharField(max_length=100)
     content = models.TextField()
     create_date = models.DateTimeField()
-
+    writer = models.CharField(max_length=10)
