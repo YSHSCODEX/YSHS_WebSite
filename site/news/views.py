6cc155852d75da93a482e7b32fcfab48290f586e
gitsokyumin <kyumin3089@gmail.com>
2024-11-11 21:16:35 +0900

    2024.11.11_1

--- site/news/views.py
+++ site/news/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import contents
 
-# Create your views here.
+def index(request):
+    news_list = contents.objects.order_by('-create_date')
+    context = {'news_list': news_list}
+    return render(request, 'news/news_list.html ')
