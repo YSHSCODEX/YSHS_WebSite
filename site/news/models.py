0d0941c8c9ac8df19544f132c5bb96c9d9ec2fde
gitsokyumin <kyumin3089@gmail.com>
2024-11-11 17:50:44 +0900

    2024.11.11

--- site/news/models.py
+++ site/news/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class contents(models.Model):
+    subject = models.CharField(max_length=100)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+
